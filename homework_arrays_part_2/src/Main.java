21719b13a0c12b4abbffa1a7aaae435561bcf38f
Taumi62831853071 <113852275+Taumi62831853071@users.noreply.github.com>
2022-10-19 18:09:47 +0300

    Add files via upload

---
+++ homework_arrays_part_2/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++)  {
+            arr[i] = random.nextInt(100_000) + 100_000;
+                System.out.println("Сумма трат за месяц составила " + arr[i] + " рублей");
+        }
+        int minExpenditure = 200000;
+        for (int i = 0; i < arr.length; i++) {
+            final int current = arr[i];
+            if (current < minExpenditure) {
+                minExpenditure = current;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minExpenditure + " рублей");
+
+        int maxExpenditure = 100000;
+        for (int i = 0; i < arr.length; i++) {
+            final int current = arr[i];
+            if (current > maxExpenditure) {
+                maxExpenditure = current;
+            }
+        }
+        System.out.println("Максимальная сумма трат за день составила " + maxExpenditure + " рублей");
+
+        int summ = 0;
+        int medExpenditure = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summ = summ + arr[i];
+            medExpenditure = summ / arr.length;
+        }
+        System.out.println("Средняя сумма трат за день составила " + medExpenditure + " рублей");
+
+        char [] reverseFullName = {'n','a','v','I',' ','v','o','n','a','v','I'};
+        for (int i = reverseFullName.length; i > 0; i--) {
+            System.out.print(reverseFullName[i-1]);
+        }
+        System.out.println();
+
+    }
+}
